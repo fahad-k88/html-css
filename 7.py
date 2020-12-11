88a037bf665878385076af3400bd8e811640d7f2
fahad-k88 <75846835+fahad-k88@users.noreply.github.com>
2020-12-11 18:12:03 +0300

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,21 @@
+n = int(input("Enter the no. of students: "))
+lst = []
+
+for i in range(n):
+    name = str(input("Enter the name of a student: "))
+    lst.append(name)
+for i in lst:
+  t = len(i)  
+  if i[t]==i.find(A) or i.find(a):
+        print(i)
+else:
+    print("No names including A or a(small) are found")
+
+
+    
+
+    
+    
+    
+    
+   
